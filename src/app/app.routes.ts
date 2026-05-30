c6d80fdc1900bc71ac55b809e4ba16a45b5bc9fb
Melina Giri <melig9539@gmail.com>
2026-05-30 09:37:15 +0545

    Initial Angular migration for MG Web Solutions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,52 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home')
+        .then(m => m.Home)
+  },
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./pages/services/services')
+        .then(m => m.Services)
+  },
+  {
+    path: 'process',
+    loadComponent: () =>
+      import('./pages/process/process')
+        .then(m => m.Process)
+  },
+  {
+    path: 'pricing',
+    loadComponent: () =>
+      import('./pages/pricing/pricing')
+        .then(m => m.Pricing)
+  },
+  {
+    path: 'portfolio',
+    loadComponent: () =>
+      import('./pages/portfolio/portfolio')
+        .then(m => m.Portfolio)
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about')
+        .then(m => m.About)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact')
+        .then(m => m.Contact)
+  },
+
+  // fallback route
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
