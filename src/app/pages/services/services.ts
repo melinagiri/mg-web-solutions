7ac1a9bb65c345d000f32db0c0b969bdb80fcb74
Melina Giri <melig9539@gmail.com>
2026-06-14 20:30:15 +0545

    melina-1: service done

--- src/app/pages/services/services.ts
+++ src/app/pages/services/services.ts
@@ -1,11 +1,11 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { TiltDirective } from '../../core/directives/tilt.directive';
 import { ScrollAnimationService } from '../../core/services/scroll-animation.service';
+declare const lucide: any;
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [TiltDirective],
+  imports: [],
   templateUrl: './services.html',
   styleUrls: ['./services.css']
 })
@@ -13,6 +13,7 @@ export class Services implements AfterViewInit {
   constructor(private scrollAnim: ScrollAnimationService) {}
 
   ngAfterViewInit(): void {
+    lucide.createIcons();
     setTimeout(() => this.scrollAnim.init(), 50);
   }
 }
